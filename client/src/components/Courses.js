115f0e5be4d008c482f57d1844d4b7a0fa1fdaa5
Bigjames728 <bigjames728@gmail.com>
2021-07-05 12:10:37 -0700

    Cleaned up code and trying to get correct functionality

--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -8,32 +8,32 @@ import Header from './Header';
 
 function Courses() {
 
-    const [courses, setCourses] = useState([]);
-  
-    useEffect(() => {
-      fetch('http://localhost:5000/api/courses')
-        .then(response => response.json())
-        .then(responseData => {
-          setCourses(responseData);
-        })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }, [])
-  
-    return(
-        <React.Fragment>
-            <Header></Header>
-                <div className="wrap main--grid">
-                    {courses.map(course => {
-                        return  <Link className="course--module course--link" to={`/courses/${course.id}`} key={`course `+ course.id}>
-                                    <h2 className="course--label">Course</h2>
-                                    <h3 className="course--title">{course.title}</h3>
-                                </Link>
-                    })}
-                </div>
-        </React.Fragment>
-    )
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/courses')
+      .then(response => response.json())
+      .then(responseData => {
+        setCourses(responseData);
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+      });
+  }, [])
+
+  return(
+    <React.Fragment>
+        <Header></Header>
+            <div className="wrap main--grid">
+                {courses.map(course => {
+                    return  <Link className="course--module course--link" to={`/courses/${course.id}`} key={`course `+ course.id}>
+                                <h2 className="course--label">Course</h2>
+                                <h3 className="course--title">{course.title}</h3>
+                            </Link>
+                })}
+            </div>
+    </React.Fragment>
+  )
 }
 
 export default Courses;
