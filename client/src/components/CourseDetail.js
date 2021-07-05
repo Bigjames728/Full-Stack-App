ef4a78e9d56ed231eb91da7e19ca53dd85422743
Bigjames728 <bigjames728@gmail.com>
2021-07-05 13:44:01 -0700

    Trying to get the const variables in useState right

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,39 +1,62 @@
 import React, { useState, useEffect } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useHistory } from 'react-router-dom';
 
 import Header from './Header';
 
 
 function CourseDetail() {
 
-    const [course, setCourse] = useState([]);
-    const { id } = useParams();
-  
-    useEffect(() => {
-      fetch(`http://localhost:5000/api/courses/${id}`)
-        .then(response => response.json())
-        .then(responseData => {
+  // const data = new Data();
+
+  const [course, setCourse] = useState("");
+  const [courseDescription, setCourseDescription] = useState("");
+  const [materialsNeeded, setMaterialsNeeded] = useState("");
+  const { id } = useParams();
+
+  const history = useHistory();
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/courses/${id}`)
+      .then(response => {
+        if (response.status === 404) {
+          return history.push("/notfound")
+        } else if (response.status === 500) {
+          throw new Error()
+        } else {
+          return response.json()
+        }
+      })
+      .then(responseData => {
+        if(responseData) {
           setCourse(responseData);
-        })
-        .catch(error => {
-          console.log('Error fetching and parsing data', error);
-        });
-    }, [])
-  
-    return(
-        <React.Fragment>
-            <Header></Header>
-            <main>
-              <div className="actions--bar">
-                <div className="wrap">
-                  <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
-                  <Link className="button" to={`/`} onClick={handleDelete}>Delete Course</Link>
-                  <Link className="button" to="/">Return to List</Link>
-                </div>
-              </div>
-            </main>
-        </React.Fragment>
-    )
+          if (responseData.description) {
+            setCourseDescription(responseData.description);
+          }
+          if (responseData.materialsNeeded) {
+            setMaterialsNeeded(responseData.materialsNeeded);
+          }
+        }
+      })
+      .catch(error => {
+        console.log('Error fetching and parsing data', error);
+        history.push('/error');
+      });
+  }, [id, history])
+
+  return(
+    <React.Fragment>
+        <Header></Header>
+        <main>
+          <div className="actions--bar">
+            <div className="wrap">
+              <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
+              {/* <Link className="button" to={`/`} onClick={handleDelete}>Delete Course</Link> */}
+              <Link className="button" to="/">Return to List</Link>
+            </div>
+          </div>
+        </main>
+    </React.Fragment>
+  )
 }
 
 export default CourseDetail;
