66cc2dace1e0a0a124eeba77e4a52df874e1bf4b
Bigjames728 <bigjames728@gmail.com>
2021-07-05 22:51:46 -0700

    Working on delete function

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
+import Data from '../Data';
 
 import Header from './Header';
 
 
 function CourseDetail() {
 
-  // const data = new Data();
+  const data = new Data();
 
   const [course, setCourse] = useState("");
   const [courseDescription, setCourseDescription] = useState("");
@@ -14,6 +15,22 @@ function CourseDetail() {
   const { id } = useParams();
 
   const history = useHistory();
+
+  //Delete a course
+  const handleDelete = async(e) => {
+    await data.deleteCourse(`/courses/${id}`)
+      .then(async (response) => {
+        if (response === null) {
+          history.push('/')
+        } else {
+          throw new Error()
+        }
+      })
+      .catch (error => {
+        console.log(error);
+        history.push('/error');
+      })
+  }
 
   useEffect(() => {
     fetch(`http://localhost:5000/api/courses/${id}`)
@@ -50,7 +67,7 @@ function CourseDetail() {
           <div className="actions--bar">
             <div className="wrap">
               <Link className="button" to={`/courses/${course.id}/update`}>Update Course</Link>
-              {/* <Link className="button" to={`/`} onClick={handleDelete}>Delete Course</Link> */}
+              <Link className="button" to={`/`} onClick={handleDelete}>Delete Course</Link>
               <Link className="button" to="/">Return to List</Link>
             </div>
           </div>
