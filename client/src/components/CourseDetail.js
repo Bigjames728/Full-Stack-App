5f8d19063ba4e93f7537134f33a80a0ce1e815d5
Bigjames728 <bigjames728@gmail.com>
2021-07-08 15:43:47 -0700

    Made some edits to code in CourseDetail.js

--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 class CourseDetail extends Component {
 
     constructor() {
         super();
         this.state = {
-            course: []
+            course: {}
         }
     }
 
     componentDidMount() {
 
         const { data } = this.props.context;
-        const id = 
+        const id = this.props.match.params.id;
 
         data.getCourse(id)
             .then(courseData => {
@@ -30,21 +31,47 @@ class CourseDetail extends Component {
     }
 
     render() {
+        const id = this.props.match.params.id;
+
+        const {
+            title,
+            description,
+            materialsNeeded,
+            estimatedTime,
+        } = this.state.course;
         
-        // const { courses } = this.state;
-        // console.log(courses);
-        // return (
-        //     <main>
-        //         <div className="wrap main--grid">
-        //             {courses.map(course => {
-        //                 return <Link className="course--module course--link" to={`/courses/${course.id}`} key={course.id.toString()}>
-        //                     <h2 className="course--label">Course</h2>
-        //                     <h3 className="course--title">{course.title}</h3>
-        //                 </Link>
-        //             })}
-        //         </div>
-        //     </main>
-        // )
+        return (
+            <main>
+                <div className="actions--bar">
+                    <div className="wrap">
+                        <Link className="button" to={`/courses/${id}/update`}>Update Course</Link>
+                        <Link className="button" to="/">Delete Course</Link>
+                        <Link className="button" to="/">Return to List</Link>
+                    </div>
+                </div>
+                <div className="wrap">
+                    <h2>Course Detail</h2>
+                    <form>
+                        <div className="main--flex">
+                            <div>
+                                <h3 className="course--detail--title">Course</h3>
+                                <h4 className="course--name">{title}</h4>
+                                <p>By </p>
+                                <p>{description}</p>
+                            </div>
+                            <div>
+                                <h3 className="course--detail--title">Estimated Time</h3>
+                                <p>{estimatedTime}</p>
+                                <h3 className="course--detail--title">Materials Needed</h3>
+                                <ul className="course--detail--list">
+                                    <ReactMarkdown>{materialsNeeded}</ReactMarkdown>
+                                </ul>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </main>
+        )
     }
 }
 
