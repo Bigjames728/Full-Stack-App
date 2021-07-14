c1b226e6bdc7fec10f755c83ad740aad8f21a26a
Bigjames728 <bigjames728@gmail.com>
2021-07-14 14:01:41 -0700

    Fixed a typo in className

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -25,7 +25,7 @@ const Form = (props) => {
 
         if (errors.length) {
             errorsDisplay = (
-                <div className="validation-errors">
+                <div className="validation--errors">
                     <h3>Validation Errors</h3>
                     <ul>
                         {errors.map((error, i) => <li key={i}>{error}</li>)}
