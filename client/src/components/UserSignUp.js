3ec8c61b8e04353fbf0fb4eace0e327848d0a178
Bigjames728 <bigjames728@gmail.com>
2021-07-14 14:32:54 -0700

    Attempted to fix the validation not showing up

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -108,7 +108,7 @@ export default class UserSignUp extends Component {
     submit = () => {
         const { context } = this.props;
 
-        const { firstName, lastName, emailAddress, password, confirmPassword } = this.state;
+        const { firstName, lastName, emailAddress, password, confirmPassword, errors } = this.state;
 
         // New user payload
         const user = {
@@ -118,7 +118,7 @@ export default class UserSignUp extends Component {
         password
         };
 
-        if (password === confirmPassword) { // Check to see if password and confirmPassword match
+        if (errors || password === confirmPassword) { // Check to see if password and confirmPassword match
 
             // If they do match, create user
             context.data.createUser(user)
