0f48e40ea157dc67e984d8ac9658fe6d2d9b3d75
Bigjames728 <bigjames728@gmail.com>
2021-07-21 16:23:13 -0700

    Imported UserSignOut, PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -11,16 +11,19 @@ import CourseDetail from './components/CourseDetail';
 import Header from './components/Header';
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
+import UserSignOut from './components/UserSignOut';
 import NotFound from './components/NotFound';
 
 //import context
 import withContext from './Context';
+import PrivateRoute from './PrivateRoute';
 
 // Give components context
 const CoursesWithContext = withContext(Courses);
 const CourseDetailWithContext = withContext(CourseDetail);
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
+const UserSignOutWithContext = withContext(UserSignOut);
 const HeaderWithContext = withContext(Header);
 
 
@@ -33,10 +36,11 @@ function App() {
     <Router>
       <HeaderWithContext />
       <Switch>
-        <Route exact path="/" component={CoursesWithContext} />
+        <PrivateRoute exact path="/" component={CoursesWithContext} />
         <Route exact path="/courses/:id" component={CourseDetailWithContext} />
         <Route path="/signin" component={UserSignInWithContext} />
         <Route path="/signup" component={UserSignUpWithContext} />
+        <Route path="/signout" conmponent={UserSignOutWithContext} />
         <Route component={NotFound} />
       </Switch>
     </Router>
