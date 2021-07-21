7a5bdddb810ecbfefc133febfe79a4e075643d70
Bigjames728 <bigjames728@gmail.com>
2021-07-20 19:00:22 -0700

    Subscribed Header to context and created an authUser state

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 
 class Header extends Component {
     render() {
+        const { context } = this.props;
+        const authUser = context.authenticatedUser;
         return (
             <header>
                 <div className="wrap header--flex">
@@ -11,8 +13,17 @@ class Header extends Component {
                     </h1>
                     <nav>
                         <ul className="header--signedout">
-                            <li><Link to="/signup">Sign Up</Link></li>
-                            <li><Link to="/signin">Sign In</Link></li>
+                            {authUser ?
+                                <React.Fragment>
+                                    <span>Welcome, {authUser.firstName}! </span>
+                                    <li><Link to="/signout">Sign Out</Link></li>
+                                </React.Fragment>
+                            :
+                                <React.Fragment>
+                                    <li><Link to="/signup">Sign Up</Link></li>
+                                    <li><Link to="/signin">Sign In</Link></li>
+                                </React.Fragment>
+                            }
                         </ul>
                     </nav>
                 </div> 
